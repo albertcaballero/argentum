20b724d83d3f2ffd685e16882d7f2118480963cc
Albert Caballero <albertcaballerocoll@gmail.com>
2025-05-08 20:02:57 +0200

    correct display of table, start popups

--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,8 @@
 function loadExpenses(expenses){
     const expensesTableBody = document.getElementById('recurringExpensesBody');
     expensesTableBody.innerHTML = ''; // Clear previous entries
+    let totalM = 0;
+    let totalY = 0;
     expenses.forEach(expense => {
       const row = document.createElement('tr');
       row.innerHTML = `
@@ -9,12 +11,25 @@ function loadExpenses(expenses){
         <td>${expense.concept}</td>
         <td>${expense.sender}</td>
         <td>${expense.receiver}</td>
-        <td>${expense.amount}</td>
+        <td>${expense.amountMonth}</td>
         <td>${expense.amountYear}</td>
         <td>${expense.date}</td>
       `;
       expensesTableBody.appendChild(row);
+       row.addEventListener('click', () => {
+            row.classList.toggle('selected');
+       }); 
+        totalM += expenses.amountMonth;
+        totalY += expense.amountYear;
     });
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>Total</td> <td></td> <td></td> <td></td>
+        <td>${totalM}</td>
+        <td>${totalY}</td>
+        <td></td>`;
+    row.style.borderTop = '3px solid black';
+    expensesTableBody.appendChild(row);
 }
 
 
@@ -22,13 +37,5 @@ window.addEventListener('DOMContentLoaded', async () => {
    //loading from DB 
     const expenses = await window.electronAPI.getRecurringExpenses();
     loadExpenses(expenses);
-
-
-    // Adding click to rows
-    const rows = document.querySelectorAll('tr');
-    rows.forEach((row) => {
-       row.addEventListener('click', () => {
-            row.classList.toggle('selected');
-       }); 
-    });
 });
+
